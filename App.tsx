bb8e81ca0d2cae21bfc59d4a465b2ebdd929bd6e
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-04-26 10:03:53 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PrideQuestStack from './StackNavigator';
+
+function App() {
+  return <PrideQuestStack />;
+}
+
+export default App;
---
+++ src/AppManager/Params.js
@@ -0,0 +1,5 @@
+export const keyPush = '4ee5c35c-0890-44b4-b692-d6e613189a97';
+export const appID = 'id6744814642';
+export const keyApps = 'v7xCW2oiGJ5JauPXwWiS5W';
+export const targetDate = '2025-04-25';
+export const bodyLin = 'https://epic-imperious-rejoicing.space/8YsTN4eO';
